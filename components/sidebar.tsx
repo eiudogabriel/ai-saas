72170734625ab257dba479ed9966977bcfa0c02f
Eiudo Gabriel <eiudogabriel@gmail.com>
2023-08-24 21:33:35 -0300

    fix: Sidebar active

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
@@ -54,6 +55,8 @@ const routes = [
 ]
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -62,11 +65,11 @@ const Sidebar = () => {
             <Image fill alt="Logo" src="/logo.png" />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
-            Genius
+            Genius.app
           </h1>
         </Link>
         <div className="space-y-1">{routes.map((route) => (
-            <Link className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 transition" href={route.href} key={route.href}>
+            <Link className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")} href={route.href} key={route.href}>
                 <div className="flex items-center flex-1">
                     <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
                     {route.label}
